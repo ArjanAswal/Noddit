2b16eabbc8b3df91bfd8f9db9605c331fab453f9
Arjan Aswal <arjanaswal09@gmail.com>
2022-04-14 12:20:47 +0530

    Create Reply model

---
+++ src/models/replyModel.js
@@ -0,0 +1,42 @@
+const { Schema, model } = require('mongoose');
+
+const replySchema = new Schema({
+  creator: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: [true, 'Reply must belong to a creator!'],
+  },
+  comment: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Comment',
+    required: [true, 'Reply must belong to a comment!'],
+  },
+  content: {
+    type: String,
+    required: [true, 'content cannot be empty!'],
+  },
+  score: {
+    type: Number,
+    default: 0,
+  },
+  upvotes: {
+    type: Number,
+    default: 0,
+  },
+  downvotes: {
+    type: Number,
+    default: 0,
+  },
+});
+
+replySchema.pre(/^find/, function (next) {
+  this.populate({
+    path: 'creator',
+    select:
+      '-__v -passwordChangedAt -email -password -passwordChangedAt -resetPasswordToken -resetPasswordExpires -upvotedPosts -downvotedPosts -upvotedComments -downvotedComments -upvotedReplies -downvotedReplies',
+  });
+
+  next();
+});
+
+module.exports = model('Reply', replySchema);
