c839edd1389ebba6ff394bd4c1645013d24485d9
Arjan Aswal <arjanaswal09@gmail.com>
2022-04-14 12:16:03 +0530

    Create Community model

---
+++ src/models/communityModel.js
@@ -0,0 +1,69 @@
+const { Schema, model } = require('mongoose');
+
+const communitySchema = new Schema({
+  creator: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: [true, 'Community must belong to a creator!'],
+  },
+  name: {
+    type: String,
+    required: [true, 'Community must have a name!'],
+    unique: true,
+  },
+  moderators: {
+    type: [mongoose.Schema.Types.ObjectId],
+    ref: 'User',
+    default: [],
+  },
+  bannedUsers: {
+    type: [mongoose.Schema.Types.ObjectId],
+    ref: 'User',
+    default: [],
+  },
+  rules: {
+    type: String,
+  },
+  avatar: {
+    type: String,
+    default: '',
+  },
+  cover: {
+    type: String,
+    default: '',
+  },
+  description: {
+    type: String,
+  },
+  welcomeMessage: {
+    type: String,
+  },
+  userFlairs: {
+    type: [String],
+  },
+  postFlairs: {
+    type: [String],
+  },
+  score: {
+    type: Number,
+    default: 0,
+  },
+});
+
+communitySchema.pre(/^find/, function (next) {
+  //   this.populate({
+  //     path: 'creator',
+  //     select:
+  //       '-__v -passwordChangedAt -email -password -passwordChangedAt -resetPasswordToken -resetPasswordExpires -upvotedPosts -downvotedPosts -upvotedComments -downvotedComments -upvotedReplies -downvotedReplies',
+  //   });
+
+  //   this.populate({
+  //     path: 'moderators',
+  //     select:
+  //       '-__v -passwordChangedAt -email -password -passwordChangedAt -resetPasswordToken -resetPasswordExpires -upvotedPosts -downvotedPosts -upvotedComments -downvotedComments -upvotedReplies -downvotedReplies',
+  //   });
+
+  next();
+});
+
+module.exports = model('Community', communitySchema);
