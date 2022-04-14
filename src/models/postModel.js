6500bebe233cd34e4289fc12da0cd892ed2146a7
Arjan Aswal <arjanaswal09@gmail.com>
2022-04-14 12:19:00 +0530

    Create Post Model

---
+++ src/models/postModel.js
@@ -0,0 +1,51 @@
+const { Schema, model } = require('mongoose');
+
+const postSchema = new Schema({
+  creator: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: [true, 'Post must belong to a creator!'],
+  },
+  title: {
+    type: String,
+    required: [true, 'Title cannot be empty!'],
+  },
+  community: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Community',
+    required: [true, 'Post must belong to a community!'],
+  },
+  description: {
+    type: String,
+  },
+  mediaURLs: [String],
+  score: {
+    type: Number,
+    default: 0,
+  },
+  upvotes: {
+    type: Number,
+    default: 0,
+  },
+  downvotes: {
+    type: Number,
+    default: 0,
+  },
+});
+
+postSchema.pre(/^find/, function (next) {
+  this.populate({
+    path: 'creator',
+    select:
+      '-__v -passwordChangedAt -email -password -passwordChangedAt -resetPasswordToken -resetPasswordExpires -upvotedPosts -downvotedPosts -upvotedComments -downvotedComments -upvotedReplies -downvotedReplies',
+  });
+
+  this.populate({
+    path: 'community',
+    select: '-__v -bannedUsers',
+  });
+
+  next();
+});
+
+module.exports = model('Post', postSchema);
