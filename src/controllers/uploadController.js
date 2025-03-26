71b5bf495a70728fff8b82b8d337b64f84074f90
Arjan Aswal <arjanaswal09@gmail.com>
2025-03-26 16:10:18 -0400

    Revert "chore: Add logic to save files locally"
    This reverts commit 69fa614980237edc658ecd16077f2612482024f5.

--- src/controllers/uploadController.js
+++ src/controllers/uploadController.js
@@ -4,20 +4,12 @@ const User = require('../models/userModel');
 const AppError = require('../utils/appError');
 const { v4: uuid } = require('uuid');
 
-const fs = require('fs');
-const path = require('path');
-
 const s3 = new AWS.S3({
   accessKeyId: process.env.AWS_S3_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_S3_SECRET_ACCESS_KEY,
 });
 
 exports.upload = async (req, res) => {
-
-  if (!process.env.AWS_S3_ACCESS_KEY_ID || !process.env.AWS_S3_SECRET_ACCESS_KEY) {
-    return this.uploadLocal(req, res);
-  }
-
   const user = await User.findById(req.user.id);
   const { fileExtension, fileName } = req.body;
 
@@ -45,55 +37,6 @@ exports.upload = async (req, res) => {
     data: {
       key,
       signedUrl,
-    },
-  });
-};
-
-exports.uploadLocal = async (req, res) => {
-  const user = await User.findById(req.user.id);
-  const { fileExtension, fileName, fileData } = req.body;
-
-  if (!user) {
-    throw new AppError('User not found', 404);
-  }
-
-  if (!fileExtension || !fileName) {
-    throw new AppError('fileName or fileExtension missing', 400);
-  }
-
-  if (!fileData) {
-    throw new AppError('No file data provided', 400);
-  }
-
-  // Create directory structure
-  const uploadsDir = path.join(__dirname, '../../uploads');
-  const userDir = path.join(uploadsDir, user._id.toString(), fileName.trim());
-
-  // Create directories recursively if they don't exist
-  if (!fs.existsSync(userDir)) {
-    fs.mkdirSync(userDir, { recursive: true });
-  }
-
-  // Generate unique filename
-  const uniqueFileName = `${uuid()}.${fileExtension.trim()}`;
-  const filePath = path.join(userDir, uniqueFileName);
-
-  // Save file to local storage
-  const buffer = Buffer.from(fileData, 'base64');
-  fs.writeFileSync(filePath, buffer);
-
-  // Return the path information
-  const relativePath = path.join(
-    user._id.toString(),
-    fileName.trim(),
-    uniqueFileName
-  );
-
-  res.status(200).json({
-    status: 'success',
-    data: {
-      key: relativePath,
-      path: filePath,
     },
   });
 };
